136521d9985b6fe8709af3a68c44b92981568186
jcpsantiago <exalowprofile@gmail.com>
2017-03-08 11:15:14 +0100

    Added scripts

--- R/QUICKI.R
+++ R/QUICKI.R
@@ -1,36 +1,27 @@
-# QUICKI
-
-#QUICKI = function(g, i){
-#  1/(log(i) + log(g))
-#}
-
-#QUICKI_DF = function(df){
-#  df %<>%
-#    select(c(subjectNumber, treatment, hour, parameter, value))
-#  
-#  df %>%
-#    filter(!(is.na(value))) %>%
-#    filter(hour == "07:15") %>%
-#    spread(parameter, value) %>%
-#    group_by(subjectNumber, treatment) %>%
-#    summarise(quicki_index = QUICKI(glucose_mgdl, insulin_microU))
-#}
-
-# REVISED QUICKI http://goo.gl/OJ5OZi
-
-QUICKIR = function(g, i, n){
-  1/(log(i) + log(g) + log(n))
+#' QUICKI insulin sensitivity index
+#'
+#' @param g Fasting glucose.
+#' @param i Fasting insulin.
+#'
+#' @return An integer.
+#'
+#' @export
+QUICKI = function(g, i){
+ 1/(log(i) + log(g))
 }
 
-QUICKIR_DF = function(df){
-  df %<>%
-    select(c(subjectNumber, treatment, timePoint, parameter, value))
-  
-  df %>%
-    filter(!(is.na(value))) %>%
-    filter(timePoint == 0) %>%
-    spread(parameter, value) %>%
-    mutate(nefa_mmol = nefa_umolL/1000) %>%
-    group_by(subjectNumber, treatment) %>%
-    summarise(indexValue = QUICKIR(glucose_mgdl, insulin_microU, nefa_mmol))
+
+#' Revised QUICKI insulin sensitivity index
+#'
+#' Source: http://goo.gl/OJ5OZi
+#'
+#' @param g Fasting glucose.
+#' @param i Fasting insulin.
+#' @param n Fasting NEFA.
+#'
+#' @return An integer.
+#'
+#' @export
+QUICKIR = function(g, i, n){
+  1/(log(i) + log(g) + log(n))
 }
--- R/matsuda.R
+++ R/matsuda.R
@@ -1,25 +1,19 @@
-#### modified from original code by Frederik Weber
-
-# ---- matsudaFunction ----
-#matsuda = function(g, i){
- # weights = c(1,2,2,2,1)
- # 80000/sqrt(g[1] * i[1] * sum(weights * g) * sum(weights * i))
-#}
-
+#' Matsuda Index.
+#'
+#' This function will calculate the Matsuda insulin sensitivity index.
+#'
+#' @param g A vector with ordered glucose values.
+#' @param i A vector with ordered insulin values.
+#'
+#' @return An integer.
+#' @examples
+#' glu <- c(4.6, 7.2, 9.8, 6.8, 5.16)
+#' ins <- c(33, 263, 797, 416, 274)
+#' mat_isi <- matsuda(glu, ins)
+#'
+#'
+#'
+#' @export
 matsuda = function(g, i){
   10000/sqrt(g[1] * i[1] * mean(g) * mean(i))
-}
-
-matsuda_df_new = function(df){
-  require(magrittr)
-  df %<>%
-    select(c(subjectNumber, treatment, timePoint, parameter, value))
-    
-  valid_matsuda_timePoints = c(0,30,60,90,120)
-  
-  df %>%
-    filter(timePoint %in% valid_matsuda_timePoints) %>%
-    spread(parameter, value) %>%
-    group_by(subjectNumber, treatment) %>%
-    summarise(indexValue = matsuda(glucose_mmoll, insulin_pmoll))
 }
