5b8d3dfb575e61384f7f268c230b580d5590dfab
jcpsantiago <exalowprofile@gmail.com>
2017-03-14 15:12:13 +0100

    Wrap functions and add sanity checks.

--- R/ogtt-based.R
+++ R/ogtt-based.R
@@ -1,93 +1,71 @@
-#' Cederholm insulin sensitivity index
+#' Calculate insulin sensitivity from OGTT values
 #'
-#' @param bw Body weight(kg).
-#' @param g Vector with glucose values.
-#' @param i Vector with insulin values.
-#'
-#' @return An integer.
-#' @export
-cederholm <- function(bw, g, i){
-  (75000 + (g[1]-g[5]) * 1.15 * 180 * 0.19 * bw)/(120 * log(mean(i) * mean(g)))
-}
-
-
-#' Gutt insulin sensitivity index
-#'
-#' @param bw Body weight (kg)
-#' @param g Vector with glucose values.
-#' @param i Vector with insulin values.
-#'
-#' @return An integer.
-#' @export
-gutt <- function(bw, g, i ){
-  (75000 + (g[1]-g[5]) * 0.19 * bw)/(120 * log((i[1] + i[5])/2) * ((g[1] + g[5])/2))
-}
-
-
-#' Matsuda Index.
-#'
-#' Calculate the Matsuda insulin sensitivity index.
-#'
-#' @param g A vector with ordered glucose values.
-#' @param i A vector with ordered insulin values.
-#'
-#' @return An integer.
-#' @examples
-#' glu <- c(4.6, 7.2, 9.8, 6.8, 5.16)
-#' ins <- c(33, 263, 797, 416, 274)
-#' isi_value <- matsuda(glu, ins)
-#'
-#'
-#'
-#' @export
-matsuda <- function(g, i){
-  10000/sqrt(g[1] * i[1] * mean(g) * mean(i))
-}
-
-
-#' NEFA-ISI insulin sensitivity index
-#'
-#' Described by Wagner at al., 2016 (http://goo.gl/cwJNz7)
-#'
+#' @param g Glucose.
 #' @param i Insulin.
-#' @param n NEFA.
-#' @param bmi BMI.
-#'
-#' @return An integer.
-#'
-#' @export
-NEFA_ISI <- function(bmi, i, n){
-  60*exp(3.853 - 0.9*log(bmi[1])
-         -0.205*log(i[1])
-         -0.128*log(i[2])
-         -0.256*log(i[3])
-         -0.138*log(n[3])
-  )
-}
-
-
-#' Stumvoll Insulin Sensitivity Index
-#'
+#' @param nefa NEFA.
 #' @param bmi Body-mass index.
-#' @param g Glucose at 90min.
-#' @param i Insulin at 120min.
+#' @param bw Body weight in kilos.
+#' @param index Insulin sensitivity index as a string. This can be one of "Cederholm",
+#'     "Gutt" (aka "ISI 0,120"), "Matsuda", "NEFA-ISI", "Stumvoll-ISI" or "Stumvoll-MCR".
 #'
 #' @return An integer.
 #' @export
-stumvoll_ISI <- function(bmi, g, i){
-  0.226 - 0.0032 * bmi - 0.0000645 * i - 0.00375 * g
-}
+ISI_OGTT <- function(glu = NA, ins = NA, nefa = NA,
+                     bmi = NA, bw = NA, index = NA){
+
+  # Only allow implemented indexes.
+  if(!(index %in% c("Cederholm", "ISI 0,120", "Matsuda", "NEFA-ISI", "Stumvoll-ISI", "Stumvoll-MCR")))
+    stop("Plese provide a valid index. For a list of implemented indexes please see ?ISIcalc")
+
+  if(length(bmi) > 1)
+    stop("Input only one value for BMI. Using unique() or subsetting (x[1]) may be necessary.")
+
+  if(length(bw) > 1)
+    stop("Input only one value for body weight. Using unique() or subsetting (x[1]) may be necessary.")
+
+  if(length(index) > 1)
+    stop("It's not possible to calculate multiple indexes with the same function call.")
 
 
-#' Stumvoll Metabolic Clearance Rate
-#'
-#' @param bmi Body-mass Index
-#' @param i Insulin at 120min
-#' @param g Glucose at 90min
-#'
-#' @return An integer.
-#'
-#' @export
-stumvoll_MCR <- function(bmi, i, g){
-  18.8 - 0.271 * bmi - 0.0052 * i - 0.27 * g
+  ## Actually calculate indexes
+  if(index=="Cederholm"){
+    # Cederholm
+    (75000 + (glu[1]-glu[2]) * 1.15 * 180 * 0.19 * bw)/(120 * log(mean(glu) * mean(ins)))
+
+
+  } else if(index == "Gutt"){
+    # Gutt / ISI 0,120
+    # glucose uptake rate in peripheral tissues
+    m <- (75000 + ((glu[1] - glu[2]) * 0.19 * bw)) / 120
+
+    # metabolic clearance rate
+    mcr <- m/mean(glu)
+
+    # insulin sensitivity index
+    mcr/log(mean(ins))
+
+
+  } else if(index == "Matsuda"){
+    10000/sqrt(glu[1] * ins[1] * mean(glu) * mean(ins))
+
+
+  } else if(index == "NEFA-ISI"){
+    ## NEFA-ISI
+    60*exp(3.853 - 0.9*log(bmi)
+           -0.205*log(ins[1])
+           -0.128*log(ins[2])
+           -0.256*log(ins[3])
+           -0.138*log(nefa[3])
+    )
+
+  } else if(index == "Stumvoll-ISI"){
+    # Stumvoll-ISI
+    0.226 - (0.0032 * bmi) - (0.0000645 * ins) - (0.00375 * glu)
+
+  } else {
+    # Stumvoll-MCR
+    18.8 - (0.271 * bmi) - (0.0052 * ins) - (0.27 * glu)
+  }
+
 }
+
